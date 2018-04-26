25e32e243e364b0eaedecf929da3380439ed2a98
Andrew Allen <andrew.allen@regence.com>
2018-04-26 14:50:26 -0700

    don't rmember what was worked on last. pushing and transferring to separate machine

--- initialize-lancache.sh
+++ initialize-lancache.sh
@@ -1,26 +1,26 @@
 #!/usr/bin/env bash
-echo "## -------------------------"
-echo "##"
-echo "## Setting Variables"
-echo "##"
-echo "## -------------------------"
-echo
+sudo echo "## -------------------------"
+sudo echo "##"
+sudo echo "## Setting Variables"
+sudo echo "##"
+sudo echo "## -------------------------"
+sudo echo
 
 lc_base_folder=./data/
 lc_hn=$( hostname )
 lc_eth_netmask=255.255.255.0
 lc_date=$( date +"%m-%d-%y %T" )
 
-echo "## -------------------------"
-echo "##"
-echo "## Checking if NPLAN is installed; If it is, uninstall it and install IFUPDOWN"
-echo "## NPLAN is ubuntu's new network manager as of 17.10. It does not support virtual addressing."
-echo "## example: eth0:0 is not supported, and there's no current plan to support it in NPLAN"
-echo "##"
-echo "## Please provide Sudo privileges"
-echo "##"
-echo "## -------------------------"
-echo
+sudo echo "## -------------------------"
+sudo echo "##"
+sudo echo "## Checking if NPLAN is installed; If it is, uninstall it and install IFUPDOWN"
+sudo echo "## NPLAN is ubuntu's new network manager as of 17.10. It does not support virtual addressing."
+sudo echo "## example: eth0:0 is not supported, and there's no current plan to support it in NPLAN"
+sudo echo "##"
+sudo echo "## Please provide Sudo privileges"
+sudo echo "##"
+sudo echo "## -------------------------"
+sudo echo
 
 if [[ $(apt list --installed | grep nplan) ]]; then
     sudo apt-get install ifupdown -y > /dev/null
@@ -28,110 +28,110 @@ if [[ $(apt list --installed | grep nplan) ]]; then
     sudo apt-get purge nplan -y >/dev/null
 fi
 
-echo "## -------------------------"
-echo "##"
-echo "## Checking if CURL is installed; If not, installing it"
-echo "##"
-echo "## -------------------------"
-echo
+sudo echo "## -------------------------"
+sudo echo "##"
+sudo echo "## Checking if CURL is installed; If not, installing it"
+sudo echo "##"
+sudo echo "## -------------------------"
+sudo echo
 
 if [ ! -f "/usr/bin/curl" ]; then
 	sudo apt-get install curl -y >/dev/null
 fi
 
-echo "## -------------------------"
-echo "##"
-echo "## Checking if DOCKER is installed; If not, installing it"
-echo "##"
-echo "## -------------------------"
-echo
+sudo echo "## -------------------------"
+sudo echo "##"
+sudo echo "## Checking if DOCKER is installed; If not, installing it"
+sudo echo "##"
+sudo echo "## -------------------------"
+sudo echo
 
 if [ ! -f "/usr/bin/docker" ]; then
     sudo curl -sSL https://get.docker.com | sudo bash
     sudo usermod -aG docker "$USER"
 fi
 
-echo "## -------------------------"
-echo "##"
-echo "## Grabbing latest docker-compose"
-echo "##"
-echo "## -------------------------"
-echo
+sudo echo "## -------------------------"
+sudo echo "##"
+sudo echo "## Grabbing latest docker-compose"
+sudo echo "##"
+sudo echo "## -------------------------"
+sudo echo
 
 latest_compose_url=$(curl -s -L https://github.com/docker/compose/releases/latest | grep -m 1 -E -o "/docker/compose/releases/download/[0-9\.]*/docker-compose-$(uname -s)-$(uname -m)")
 sudo echo $latest_compose_url
 sudo curl -o /usr/local/bin/docker-compose -L "http://www.github.com$latest_compose_url"
 sudo chmod +x /usr/local/bin/docker-compose
 
-echo "## -------------------------"
-echo "##"
-echo "## Creating Temporary Folders"
-echo "##"
-echo "## -------------------------"
-echo
+sudo echo "## -------------------------"
+sudo echo "##"
+sudo echo "## Creating Temporary Folders"
+sudo echo "##"
+sudo echo "## -------------------------"
+sudo echo
 
 mkdir -p $lc_base_folder/config/
 mkdir -p $lc_base_folder/temp
 mkdir -p $lc_base_folder/temp/unbound/
 
-echo "## -------------------------"
-echo "##"
-echo "## Setting Helper Functions"
-echo "##"
-echo "## -------------------------"
-echo
+sudo echo "## -------------------------"
+sudo echo "##"
+sudo echo "## Setting Helper Functions"
+sudo echo "##"
+sudo echo "## -------------------------"
+sudo echo
 
 get_ip() {
     ## Save the Mac Addresses if not already done
     lc_list_int=$( ls /sys/class/net | grep -v lo)
-    lc_list_mac=$( cat /sys/class/net/*/address | grep -v 00:00:00:00:00:00 )
-    echo The MAC Adresses for all interfaces are:
-    echo
-    echo "$lc_list_int"
-    echo "$lc_list_mac"
-    echo
+    sudo echo "The usable interfaces for the Lancache are:"
+    sudo echo
+    sudo echo "$lc_list_int"
+    sudo echo
 
-    ## Check if the Interface is defined
-    ## If not will ask for the question
+    ## Check if the Interface to be used is defined
+    ## If not, ask to define it
     if [ ! "$lc_input_interface" ]; then
+        ## Redundant check for the interface variable, just in case the above check gives false positive.
+        ## If the variable is found, remove it
         if [ "$lc_input_interface" ]; then
             unset $lc_input_interface
         fi
 
-        echo "(Please enter the interface to use)"
-        echo The interfaces on this machine are:
-        echo "$lc_list_int"
+        sudo echo "(Please enter the interface to use)"
+        sudo echo The interfaces on this machine are:
+        sudo echo "$lc_list_int"
         read -r -p "Selected Interface: " lc_input
-        echo You have entered: "$lc_input"
+        sudo echo You have entered: "$lc_input"
         export lc_input_interface=$lc_input
-        echo
-        echo Checking if this interface exists...
-        echo
+        sudo echo
+        sudo echo Checking if this interface exists...
+        sudo echo
 
         ## Built in Check
         interface_check=$( ls /sys/class/net | grep "$lc_input_interface" >/dev/null )
         if $interface_check; then
-            echo [ "$lc_date" ] !!! SUCCESS !!!
-            echo It seems that "$lc_input_interface" exists
-            echo The user "$USER" chose the interface: "$lc_input_interface" from the following:
-            echo "$lc_list_int"
-            echo
+            sudo echo [ "$lc_date" ] !!! SUCCESS !!!
+            sudo echo It seems that "$lc_input_interface" exists
+            sudo echo The user "$USER" chose the interface: "$lc_input_interface" from the following:
+            sudo echo "$lc_list_int"
+            sudo echo
         else
-            echo [ "$lc_date" ] !!! ERROR !!!
-            echo Sorry you have entered a wrong interface...
-            echo
-            echo The user: "$USER" entered the following interface: "$lc_input_interface"
-            echo Wich doesnt exist
-            echo
-            echo These are the available interfaces "$USER" could choose from: "$lc_list_int"
-            echo Please re-run this script.
-            echo
+            sudo echo [ "$lc_date" ] !!! ERROR !!!
+            sudo echo Sorry you have entered an incorrect interface...
+            sudo echo
+            sudo echo The user: "$USER" entered the following interface: "$lc_input_interface",
+            sudo echo Wich doesnt exist
+            sudo echo
+            sudo echo These are the available interfaces "$USER" could choose from: "$lc_list_int"
+            sudo echo Please re-run this script.
+            sudo echo
             break
         fi
     fi
 
     lc_temp_ip=$(ip addr show dev "$lc_input_interface" | grep 'inet ' | grep -o '[0-9]*\.[0-9]*\.[0-9]*\.[0-9]*' | head -n 1)
-    echo Found the following IP address configured: "$lc_temp_ip"
+    sudo echo Found the following IP address configured: "$lc_temp_ip"
     read -r -p "Do you want to use this IP? " response
     if [[ "$response" =~ ^([yY][eE][sS]|[yY])+$ ]]
     then
@@ -144,9 +144,9 @@ get_ip() {
 confirm_ip() {
     ip_confirmed="false"
     until [ $ip_confirmed = "true" ]; do
-    echo
+    sudo echo
     read -r -p "What should the new IP be? " new_ip
-    echo The IP will be set to: "$new_ip"
+    sudo echo The IP will be set to: "$new_ip"
     read -r -p "Does this IP look correct? " ip_confirmation
     if [[ $ip_confirmation =~ ^([yY][eE][sS]|[yY])$ ]]
     then
@@ -158,34 +158,34 @@ confirm_ip() {
     done
 }
 
-echo "## -------------------------"
-echo "##"
-echo "## Update Lancache config from github"
-echo "##"
-echo "## -------------------------"
-echo
+sudo echo "## -------------------------"
+sudo echo "##"
+sudo echo "## Update Lancache config from github"
+sudo echo "##"
+sudo echo "## -------------------------"
+sudo echo
 
 git submodule init
 git submodule update --remote --recursive
 
-echo "## -------------------------"
-echo "##"
-echo "## Starting initialize script"
-echo "## Detecting IP"
-echo "##"
-echo "## -------------------------"
-echo
+sudo echo "## -------------------------"
+sudo echo "##"
+sudo echo "## Starting initialize script"
+sudo echo "## Detecting IP"
+sudo echo "##"
+sudo echo "## -------------------------"
+sudo echo
 
 if [[ -z $HOST_IP ]]; then
   get_ip
 
   if [[ -n $HOST_IP ]]; then
-    echo Detected configured host IP as "$HOST_IP"
-    echo Creating Interface IPs from "$HOST_IP"
-    HOST_IP_P1=$(echo "${HOST_IP}" | tr "." " " | awk '{ print $1 }')
-    HOST_IP_P2=$(echo "${HOST_IP}" | tr "." " " | awk '{ print $2 }')
-    HOST_IP_P3=$(echo "${HOST_IP}" | tr "." " " | awk '{ print $3 }')
-    HOST_IP_P4=$(echo "${HOST_IP}" | tr "." " " | awk '{ print $4 }')
+    sudo echo Detected configured host IP as "$HOST_IP"
+    sudo echo Creating Interface IPs from "$HOST_IP"
+    HOST_IP_P1=$(sudo echo "${HOST_IP}" | tr "." " " | awk '{ print $1 }')
+    HOST_IP_P2=$(sudo echo "${HOST_IP}" | tr "." " " | awk '{ print $2 }')
+    HOST_IP_P3=$(sudo echo "${HOST_IP}" | tr "." " " | awk '{ print $3 }')
+    HOST_IP_P4=$(sudo echo "${HOST_IP}" | tr "." " " | awk '{ print $4 }')
     ## Increment the last IP digit for every Game
     lc_incr_steam=$((HOST_IP_P4+1))
     export lc_ip_steam=$HOST_IP_P1.$HOST_IP_P2.$HOST_IP_P3.$lc_incr_steam
@@ -238,29 +238,29 @@ if [[ -z $HOST_IP ]]; then
     lc_incr_pearlabyss=$((HOST_IP_P4+17))
     export lc_ip_pearlabyss=$HOST_IP_P1.$HOST_IP_P2.$HOST_IP_P3.$lc_incr_pearlabyss
 
-    echo Interface IPs created. Starting IP Assignments
-    echo
+    sudo echo Interface IPs created. Starting IP Assignments
+    sudo echo
   else
-    echo Could not determine set host IP. Exiting.
+    sudo echo Could not determine set host IP. Exiting.
     exit 1
   fi
 fi
 
-echo "## -------------------------"
-echo "##"
-echo "## update proxy-bind in vhosts-enabled file"
-echo "##"
-echo "## -------------------------"
-echo
+sudo echo "## -------------------------"
+sudo echo "##"
+sudo echo "## update proxy-bind in vhosts-enabled file"
+sudo echo "##"
+sudo echo "## -------------------------"
+sudo echo
 ## Change the Proxy Bind in Lancache Configs
 sudo sed -i 's|lc-host-proxybind|'$lc_ip'|g' ./lancache/conf/vhosts-enabled/*.conf
 
-echo "## -------------------------"
-echo "##"
-echo "## Make the Necessary Changes For The New Host File"
-echo "##"
-echo "## -------------------------"
-echo
+sudo echo "## -------------------------"
+sudo echo "##"
+sudo echo "## Make the Necessary Changes For The New Host File"
+sudo echo "##"
+sudo echo "## -------------------------"
+sudo echo
 
 sudo cp ./lancache/hosts $lc_base_folder/temp/hosts
 sudo sed -i "s|lc-host-apple|$lc_ip_apple|g" $lc_base_folder/temp/hosts
@@ -283,12 +283,12 @@ sudo sed -i "s|lc-host-wargaming|$lc_ip_wargaming|g" $lc_base_folder/temp/hosts
 sudo sed -i "s|lc-host-zenimax|$lc_ip_zenimax|g" $lc_base_folder/temp/hosts
 sudo sed -i "s|lc-hostname|$lc_hn|g" $lc_base_folder/temp/hosts
 
-echo "## -------------------------"
-echo "##"
-echo "## Make the Necessary Changes For The New Interfaces File"
-echo "##"
-echo "## -------------------------"
-echo
+sudo echo "## -------------------------"
+sudo echo "##"
+sudo echo "## Make the Necessary Changes For The New Interfaces File"
+sudo echo "##"
+sudo echo "## -------------------------"
+sudo echo
 
 sudo cp ./lancache/interfaces $lc_base_folder/temp/interfaces
 sudo sed -i "s|lc-host-apple|$lc_ip_apple|g" $lc_base_folder/temp/interfaces
@@ -312,12 +312,12 @@ sudo sed -i "s|lc-host-vint|$lc_input_interface|g" $lc_base_folder/temp/interfac
 sudo sed -i "s|lc-host-wargaming|$lc_ip_wargaming|g" $lc_base_folder/temp/interfaces
 sudo sed -i "s|lc-host-zenimax|$lc_ip_zenimax|g" $lc_base_folder/temp/interfaces
 
-echo "## -------------------------"
-echo "##"
-echo "## Preparing configuration for unbound"
-echo "##"
-echo "## -------------------------"
-echo
+sudo echo "## -------------------------"
+sudo echo "##"
+sudo echo "## Preparing configuration for unbound"
+sudo echo "##"
+sudo echo "## -------------------------"
+sudo echo
 
 sudo cp ./lancache/unbound/unbound.conf $lc_base_folder/temp/unbound/
 sudo sed -i "s|lc-host-apple|$lc_ip_apple|g" $lc_base_folder/temp/unbound/unbound.conf
@@ -340,18 +340,18 @@ sudo sed -i "s|lc-host-uplay|$lc_ip_uplay|g" $lc_base_folder/temp/unbound/unboun
 sudo sed -i "s|lc-host-wargaming|$lc_ip_wargaming|g" $lc_base_folder/temp/unbound/unbound.conf
 sudo sed -i "s|lc-host-zenimax|$lc_ip_zenimax|g" $lc_base_folder/temp/unbound/unbound.conf
 
-echo "## -------------------------"
-echo "##"
-echo "## Moving Base Files to The Correct Locations"
-echo "##"
-echo "## -------------------------"
-echo
+sudo echo "## -------------------------"
+sudo echo "##"
+sudo echo "## Moving Base Files to The Correct Locations"
+sudo echo "##"
+sudo echo "## -------------------------"
+sudo echo
 
 if [ -f "$lc_base_folder/temp/hosts" ]; then
 	sudo mv /etc/hosts /etc/hosts.bak
 	sudo cp $lc_base_folder/temp/hosts /etc/hosts
 	else
-	echo Could not find "$lc_base_folder/temp/hosts". Exiting.
+	sudo echo Could not find "$lc_base_folder/temp/hosts". Exiting.
 	exit 1
 fi
 
@@ -359,7 +359,7 @@ if [ -f "$lc_base_folder/temp/interfaces" ]; then
 	sudo mv /etc/netork/interfaces /etc/netork/interfaces.bak
 	sudo mv $lc_base_folder/temp/interfaces /etc/network/interfaces
 	else
-	echo Could not find "$lc_base_folder/temp/interfaces". Exiting.
+	sudo echo Could not find "$lc_base_folder/temp/interfaces". Exiting.
 	exit 1
 fi
 
@@ -368,7 +368,7 @@ if [ -f "$lc_base_folder/temp/unbound/unbound.conf" ]; then
     sudo mv ./lancache/unbound/unbound.conf ./lancache/unbound/unbound.conf.bak
 	yes | cp $lc_base_folder/temp/unbound/unbound.conf ./lancache/unbound/unbound.conf
 	else
-	echo Could not find "$lc_base_folder/temp/unbound/unbound.conf". Exiting.
+	sudo echo Could not find "$lc_base_folder/temp/unbound/unbound.conf". Exiting.
 	exit 1
 fi
 
@@ -382,12 +382,12 @@ fi
 sudo echo "nameserver 8.8.8.8" > /etc/resolv.conf
 sudo echo "nameserver 8.8.4.4" > /etc/resolv.confc
 
-echo "## -------------------------"
-echo "##"
-echo "## Installing traffic monitoring tools"
-echo "##"
-echo "## -------------------------"
-echo
+sudo echo "## -------------------------"
+sudo echo "##"
+sudo echo "## Installing traffic monitoring tools"
+sudo echo "##"
+sudo echo "## -------------------------"
+sudo echo
 sudo apt-get install nload iftop tcpdump tshark -y
 
 ## Clean up temp folder
@@ -396,10 +396,10 @@ sudo rm -rf $lc_base_folder/temp
 ## Start Docker Containers
 sudo docker-compose up -d --build
 
-echo "## -------------------------"
-echo "##"
-echo "## Reboot system for network changes to apply"
-echo "##"
-echo "## -------------------------"
+sudo echo "## -------------------------"
+sudo echo "##"
+sudo echo "## Reboot system for network changes to apply"
+sudo echo "##"
+sudo echo "## -------------------------"
 
 #reboot
