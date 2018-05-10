e813ba15e76b5b1703abb91bdaead1e9bbd824d2
Andrew Allen <andrew.allen@regence.com>
2018-05-10 13:44:37 -0700

    missed my actual fail check

--- initialize-lancache.sh
+++ initialize-lancache.sh
@@ -78,7 +78,7 @@ echo "The current docker compose version is: " $COMPOSE_VER
 curl -L https://github.com/docker/compose/releases/download/$COMPOSE_VER/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 curl -L https://raw.githubusercontent.com/docker/compose/$COMPOSE_VER/contrib/completion/bash/docker-compose -o /etc/bash_completion.d/docker-compose
-if [ $(docker-compose --version) ]; then
+if [ ! $(docker-compose --version) ]; then
     exit 1
 fi
 echo "## -------------------------"
